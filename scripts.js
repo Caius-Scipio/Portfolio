a4735dcdd10115a5bbe757fb522b3f9a1ce7fb36
Caius Scipio <caiuscipio@gmail.com>
2023-10-06 13:34:02 +0900

    Page Color Picker added

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
+const colorThemes = document.querySelectorAll('[name="theme"]');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
@@ -10,3 +11,30 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 });
+
+// saves user selected theme to local storage
+const storeTheme = function (theme) {
+    localStorage.setItem("theme", theme);
+};
+
+// sets theme to revisiting user
+const setTheme = function () {
+    const activeTheme = localStorage.getItem("theme");
+    colorThemes.forEach((themeOption) => {
+      if (themeOption.id === activeTheme) {
+        themeOption.checked = true;
+      }
+    });
+    // fallback for no :has() support
+    document.documentElement.className = activeTheme;
+  };
+
+colorThemes.forEach((themeOption) => {
+    themeOption.addEventListener("click", () => {
+        storeTheme(themeOption.id);
+    });
+    // fallback for no :has() support
+    document.documentElement.className = themeOption.id;
+});
+
+document.onload = setTheme();
